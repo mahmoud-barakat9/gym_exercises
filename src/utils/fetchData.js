ba0e88e88ded009af711ac2962c9a12775f8970e
mahmoud <mahmoudba98a@gmail.com>
2024-02-28 13:45:36 +0300

    first

---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': 'e189e93a20msh5fee709434cc690p129a99jsna4992eb69c07',
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': 'e189e93a20msh5fee709434cc690p129a99jsna4992eb69c07',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
